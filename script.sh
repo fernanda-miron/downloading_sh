f20e88b263d6d64a0fb1e7cb341cd13b13a3cf62
fernanda-miron <maria.miront@alumno.buap.mx>
2022-01-16 19:13:28 -0600

    wget to curl

--- script.sh
+++ script.sh
@@ -9,7 +9,7 @@ while read linea
 				file_3=$(echo "$linea" | cut -f4)
 				echo "Creating dir $dir with files $file_1, $file_2 and $file_3"
 				mkdir $dir
-				wget -nc -P ./$dir/ $file_1
-				wget -nc -P ./$dir/ $file_2
-				wget -nc -P ./$dir/ $file_3
+				(cd $dir && curl -O $file_1)
+				(cd $dir && curl -O $file_2)
+				(cd $dir && curl -O $file_3)
 			done < $1
